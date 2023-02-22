6e9f358342348d11861e8f111bc2834a00d896fd
weishi <weishi@whut.edu.cn>
2023-02-22 16:45:00 +0800

    Great! Now State work with Game like a power strip.

--- include/State.hpp
+++ include/State.hpp
@@ -1,46 +1,65 @@
 #ifndef _INCLUDE_STATE_HPP_
 #define _INCLUDE_STATE_HPP_
 
+#include <memory>
+#include <functional>
+
 #include <SDL2/SDL.h>
 
 #include "handles.hpp"
 
+class Game;
+
+using HandleEventFunc = std::function<void (SDL_Event*)>;
+using UpdateFunc = std::function<void ()>;
+using DrawFunc = std::function<void (SDL_Renderer*)>;
+
+struct StateInitOptions {
+  HandleEventFunc on_handle_event;
+  UpdateFunc on_update;
+  DrawFunc on_draw;
+};
+
 class State {
 public:
-  virtual bool init() = 0;
-  virtual void handle_event(SDL_Event* event) = 0;
-  virtual void update() = 0;
-  virtual void draw(RendererHandle hrdr) = 0;
-  virtual void cleanup() = 0;
+  State(std::weak_ptr<Game> game) : game(game) {};
+  State(std::weak_ptr<Game> game, StateInitOptions options)
+  : on_handle_event(options.on_handle_event)
+  , on_update(options.on_update)
+  , on_draw(options.on_draw)
+  , game(game) {};
 
-  bool is_pause() {
-    return pause;
-  };
+  HandleEventFunc on_handle_event;
+  UpdateFunc on_update;
+  DrawFunc on_draw;
 
-  void toggle_pause(bool v) {
-    pause = v;
+  virtual bool init() { return true; };
+  virtual void handle_event(SDL_Event* event) {
+    if (on_handle_event) on_handle_event(event);
   };
+  virtual void update() {
+    if (on_update) on_update();
+  };
+  virtual void draw(RendererHandle hrdr) {
+    if (on_draw) on_draw(hrdr.get());
+  };
+  virtual void cleanup() {};
 
-  bool is_invisible() {
-    return invisible;
-  };
+  bool is_pause() { return pause; };
+  void toggle_pause(bool v) { pause = v; };
 
-  void toggle_invisible(bool v) {
-    invisible = v;
-  };
+  bool is_invisible() { return invisible; };
+  void toggle_invisible(bool v) { invisible = v; };
 
-  bool is_sleep() {
-    return sleep;
-  };
-
-  void toggle_sleep(bool v) {
-    sleep = v;
-  };
+  bool is_sleep() { return sleep; };
+  void toggle_sleep(bool v) { sleep = v; };
 
 protected:
-  bool pause{false}; // update or not
-  bool invisible{false}; // draw or not
-  bool sleep{false}; // handle_event or not
+  std::weak_ptr<Game> game;
+
+  bool pause{false}; // update or not, using by Game
+  bool invisible{false}; // draw or not, using by Game
+  bool sleep{false}; // handle_event or not, using by Game
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,61 @@
 #include <memory>
 
 #include "Game.hpp"
-#include "StateTest.hpp"
+#include "State.hpp"
+#include "userevent.hpp"
+
+struct Line {
+  Line(int row): row(row) {};
+  int row{ 1 };
+  double x{ 5.0 };
+  void update() {
+    x += 1;
+  };
+  void draw(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 215, 215, 215, 255);
+    SDL_RenderDrawLine(renderer, 0, row, x, row);
+  };
+  void on_click() {
+    x -= 5;
+  };
+};
 
 int main(int argc, char *args[]) {
-  Game game{};
+  Line line1(1);
+  Line line2(21);
 
-  WindowHandle hwnd = game.create_window("Hello",
+  auto game = std::make_shared<Game>();
+
+  WindowHandle hwnd = game->create_window("Hello",
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
     300, 300, SDL_WINDOW_RESIZABLE);
-  game.set_window_icon(hwnd, "res/icon_16x16.bmp");
+  game->set_window_icon(hwnd, "res/icon_16x16.bmp");
 
-  RendererHandle hrdr = game.create_renderer(hwnd, -1,
+  RendererHandle hrdr = game->create_renderer(hwnd, -1,
     SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
-  auto state_test = std::make_unique<StateTest>();
-  game.push_state("test", std::move(state_test));
+  StateInitOptions menu_options{
+    .on_handle_event = [&line1, &line2](SDL_Event* event) {
+      if (event->type == SDL_USEREVENT
+        && event->user.code == (int)userevent::EventCode::mouse_click) {
+        line1.on_click();
+        line2.on_click();
+      }
+    },
+    .on_update = [&line1, &line2]() {
+      line1.update();
+      line2.update();
+    },
+    .on_draw = [&line1, &line2](SDL_Renderer *renderer) {
+      line1.draw(renderer);
+      line2.draw(renderer);
+    },
+  };
 
-  game.start(hrdr);
+  auto menu = std::make_unique<State>(game, menu_options);
+
+  game->push_state("menu", std::move(menu));
+  game->start(hrdr);
 
   return 0;
 }
